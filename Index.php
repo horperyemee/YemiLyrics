94469a0d322ae7dba353bf1c20eabe5d83421b96
horperyemi <horperyemi99@gmail.com>
2017-07-14 11:40:39 -0700

    Updated Version of my README file.

---
+++ Index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Yemi Lyrics</title>
+
+	<!-- link of the stylesheet for this project -->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+</head>
+
+<!-- BODY starts Here.... -->
+<body>
+
+	<div>
+
+	<!-- Header Start HERE... -->
+		<div class="row header">
+
+			<div class="col-md-4">
+				<img src="" class="img-responsive">
+			</div>
+
+			<div class="col-md-4"></div>
+
+			<div class="col-md-4"></div>
+
+		</div>
+
+	<!-- Main Body Start HERE.... -->
+
+		<div class="">
+			
+			<form method="post" action="">
+
+				<input type="" name="">
+
+			</form>
+
+
+
+		</div>
+
+	<!-- Lower Part of the Body Start HERE.... -->
+
+		<div>
+
+			<div class="container">
+
+				<div class="row">
+
+					<div class="col-md-4">
+						
+					</div>
+					<div class="col-md-4">
+						
+					</div>
+					<div class="col-md-4">
+						
+					</div>
+
+				</div>
+
+			</div>
+
+		</div>
+
+
+
+	</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script type="text/javascript" src="bootstrap.min.js"></script>
+</body>
+</html>
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # YemiLyrics
-A website that provides lyrics for any song 
+A website that provides lyrics for any song. 
+This Project is a project that allows me show my talent using my skill and specialization.
+Project: Building a song lyrics website..
